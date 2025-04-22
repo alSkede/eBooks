7f67413448a566684e4345867fa76e930f38fb78
alSkede <147340446+alSkede@users.noreply.github.com>
2025-04-22 12:32:40 +0200

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
 import SceneViewer from './scenes/SceneViewer';
 
+import { sceneData } from './sceneDataFull';
+import { sceneInteractions } from './data/sceneInteractions';
+import SceneInteractive from './components/SceneInteractive';
+
 export default function App() {
   return (
     <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
