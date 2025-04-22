b004cfc79a0a7237f0c97b19eb8d9a0fcb226bf9
alSkede <147340446+alSkede@users.noreply.github.com>
2025-04-22 23:02:47 +0200

    Update SceneViewer.jsx

--- components/ui/SceneViewer.jsx
+++ components/ui/SceneViewer.jsx
@@ -4,7 +4,9 @@ function SceneViewer({ currentScene }) {
       <h2>{currentScene.title}</h2>
 
       <img
-        src={currentScene.visual}
+        src={currentScene.visual.includes('scene-images/}
+            ? currentScene.visual 
+            : `scene-images/${currentScene.visual}`}
         alt={currentScene.title}
         className="w-full max-w-3xl mx-auto my-4 rounded-xl shadow-md"
       />
