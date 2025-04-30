15215d4b888e3e6757e1b7c8adbc18ca4089580f
alSkede <147340446+alSkede@users.noreply.github.com>
2025-04-30 10:28:30 +0200

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className="bg-gradient-to-br from-white to-pink-100 text-gray-900">
-        {children}
-      </body>
-    </html>
-  );
-}
