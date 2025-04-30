771795da6d674968b2ccbf3ec4762185d57c5401
alSkede <147340446+alSkede@users.noreply.github.com>
2025-04-30 10:27:34 +0200

    Delete components directory

--- components/card.tsx
+++
@@ -1,5 +0,0 @@
-export const Card = ({ children, className = "" }) => (
-  <div className={`p-4 bg-white rounded shadow ${className}`}>{children}</div>
-);
-
-export const CardContent = ({ children }) => <div>{children}</div>;
