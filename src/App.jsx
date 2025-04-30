992117be2328339fb63d2861e55d7b4ae3c9d9ee
alSkede <dressler@inspective.de>
2025-04-30 15:50:35 +0200

    Antonia

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Tryll & Li Bäh</h1>
+      <p>Willkommen im interaktiven eBook!</p>
+    </div>
+  )
+}
+
+export default App
