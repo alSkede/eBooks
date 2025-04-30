ff9e45697caa9d405f1174dd2761405ae25ec87f
alSkede <147340446+alSkede@users.noreply.github.com>
2025-04-30 17:25:50 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import './styles/buttonStyles.css'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css' // optional, je nachdem ob du eine CSS-Datei nutzt
+import { EbookProvider } from './hooks/useEbook'
+import './styles/buttonStyles.css'
+import './styles/iconButtonStyles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <EbookProvider>
+      <App />
+    </EbookProvider>
   </React.StrictMode>
 )
