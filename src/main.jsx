016f46cec889476e7978716539754422ada8b145
alSkede <147340446+alSkede@users.noreply.github.com>
2025-04-30 16:23:01 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './styles/buttonStyles.css'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css' // optional, je nachdem ob du eine CSS-Datei nutzt
