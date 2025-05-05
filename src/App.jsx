b3c0eac44970a7b3bfc11d12ce3b7b8a6c3b7e3e
alSkede <147340446+alSkede@users.noreply.github.com>
2025-05-05 16:45:01 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import SceneViewer from './SceneViewer'
 import { Card, CardContent } from "./components/ui/card";
-import { Separator } from "./components/ui/separator";
 import './styles/buttonStyles.css'
 import './styles/iconButtonStyles.css'
 import './index.css'
