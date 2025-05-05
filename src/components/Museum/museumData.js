ef4b265a3ed684199079c2e4e22e64bc2fa5a9a5
alSkede <147340446+alSkede@users.noreply.github.com>
2025-05-05 21:41:38 +0200

    Update museumData.js

--- src/components/Museum/museumData.js
+++ src/components/Museum/museumData.js
@@ -1,49 +1,41 @@
 export const museumData = {
   1: {
     title: "The Puddle of Potential",
-    scene: "Scene 1",
     tryll: "Origin. Soup. Soul bath.",
     libaeh: "Organic amino-acid fluid. Recommend containment."
   },
   3: {
     title: "Fragment of Self",
-    scene: "Scene 3",
     tryll: "A piece of someone’s dream.",
     libaeh: "Recovered from subconscious debris."
   },
   5: {
     title: "Fossil Brooch of Memory",
-    scene: "Scene 5",
     tryll: "A wearable past.",
     libaeh: "Abandoned exoskeletal fragment."
   },
   6: {
     title: "The Breath of Thought",
-    scene: "Scene 6",
     tryll: "An idea becoming airborne.",
     libaeh: "Vacuum-sealed air. Possibly mucus vapor."
   },
   9: {
     title: "Anchor of Doubt",
-    scene: "Scene 9",
     tryll: "Weighted certainty.",
     libaeh: "Unverifiable but dense."
   },
   11: {
     title: "Glass Loop of Intuition",
-    scene: "Scene 11",
     tryll: "Time’s suggestion in shape.",
     libaeh: "Unclear entrance. No exit."
   },
   14: {
     title: "Thread of Parallel Thought",
-    scene: "Scene 14",
     tryll: "May connect to unseen intentions.",
     libaeh: "Warning: Unstable implications."
   },
   18: {
     title: "Refracted Farewell",
-    scene: "Scene 18",
     tryll: "A goodbye that came too early.",
     libaeh: "Emotion crystallized and bent."
   }
