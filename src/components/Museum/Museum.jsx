de18c214ed4beebde977332554c90cd25a33589e
alSkede <147340446+alSkede@users.noreply.github.com>
2025-05-05 19:43:21 +0200

    Update Museum.jsx

--- src/components/Museum/Museum.jsx
+++ src/components/Museum/Museum.jsx
@@ -1,4 +1,4 @@
-import { Card, CardContent } from "../ui/card";
+import { Card, CardContent } from "../../ui/card";
 import { useEbook } from "../../hooks/useEbook";
 import { museumData } from "./museumData";
 
