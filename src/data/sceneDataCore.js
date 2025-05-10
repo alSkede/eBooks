08d0ce6d899438d28757b68711b36a92c95c50bc
alSkede <147340446+alSkede@users.noreply.github.com>
2025-05-10 23:15:13 +0200

    Create sceneDataCore.js

---
+++ src/data/sceneDataCore.js
@@ -0,0 +1,122 @@
+export const sceneDataCore = [
+   {
+    id: "scene-01",
+    title: "Scene 1",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 1" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene01.png",
+  },
+  {
+    id: "scene-02",
+    title: "Scene 2",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 2" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene02.png",
+  },
+  {
+    id: "scene-03",
+    title: "Scene 3",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 3" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene03.png",
+  },
+  {
+    id: "scene-04",
+    title: "Scene 4",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 4" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene04.png",
+  },
+  {
+    id: "scene-05",
+    title: "Scene 5",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 5" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene05.png",
+  },
+  {
+    id: "scene-06",
+    title: "Scene 6",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 6" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene06.png",
+  },
+  {
+    id: "scene-07",
+    title: "Scene 7",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 7" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene07.png",
+  },
+  {
+    id: "scene-08",
+    title: "Scene 8",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 8" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene08.png",
+  },
+  {
+    id: "scene-09",
+    title: "Scene 9",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 9" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene09.png",
+  },
+  {
+    id: "scene-10",
+    title: "Scene 10",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 10" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene10.png",
+  },
+  {
+    id: "scene-11",
+    title: "Scene 11",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 11" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene11.png",
+  },
+  {
+    id: "scene-12",
+    title: "Scene 12",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 12" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene12.png",
+  },
+  {
+    id: "scene-13",
+    title: "Scene 13",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 13" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene13.png",
+  },
+  {
+    id: "scene-14",
+    title: "Scene 14",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 14" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene14.png",
+  },
+  {
+    id: "scene-15",
+    title: "Scene 15",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 15" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene15.png",
+  },
+  {
+    id: "scene-16",
+    title: "Scene 16",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 16" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene16.png",
+  },
+  {
+    id: "scene-17",
+    title: "Scene 17",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 17" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene17.png",
+  },
+  {
+    id: "scene-18",
+    title: "Scene 18",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 18" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene18.png",
+  },
+  {
+    id: "scene-19",
+    title: "Scene 19",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 19" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene19.png",
+  },
+  {
+    id: "scene-20",
+    title: "Scene 20",
+    narration: [{ speaker: "Narrator", text: "Narration for scene 20" }],
+    visual: "www.inspective.de/html/eBooks/Wiggle/scenes/Scene20.png",
+  }
+];
